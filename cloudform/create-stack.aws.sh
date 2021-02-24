9d08a41f27a1bee941013c8158b8ea03ab53f6b7
Philip J. Reed <reed9999@uw.edu>
2021-02-24 01:00:44 +0000

    Intermediate. Transfer to env-02 for less EC2 usage.

--- cloudform/create-stack.aws.sh
+++ cloudform/create-stack.aws.sh
@@ -2,11 +2,10 @@
 aws cloudformation describe-stacks > cfn-preexisting.json
 
 TEMPLATE_URL="https://reed9999.s3-us-west-2.amazonaws.com/automation/cfn-temp/cf02-spot.yaml"
+REGION=$1 || "us-east-2"
 aws cloudformation create-stack \
 --stack-name Philip02b-auto \
 --template-body ${TEMPLATE_URL} \
---parameters ParameterKey=keyName,ParameterValue=temp-spot-fun \
---region us-east-2 >> cfn-new-statk-id.json
-
-
+--parameters ParameterKey=keyName,ParameterValue=CapeTownKey \
+--region $REGION >> cfn-new-stack-id.json
 
