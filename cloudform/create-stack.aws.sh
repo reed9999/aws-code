3ef6f9e948fea885adc337b73035e8c3a41cb84c
Philip J. Reed <reed9999@uw.edu>
2021-02-25 01:01:35 +0000

    round out cf02 spot instances and start a new template

--- cloudform/create-stack.aws.sh
+++ cloudform/create-stack.aws.sh
@@ -4,8 +4,8 @@ aws cloudformation describe-stacks > cfn-preexisting.json
 TEMPLATE_URL="https://reed9999.s3-us-west-2.amazonaws.com/automation/cfn-temp/cf02-spot.yaml"
 REGION=$1 || "us-east-2"
 aws cloudformation create-stack \
---stack-name Philip02b-auto \
+--stack-name Philip02d-auto \
 --template-body ${TEMPLATE_URL} \
---parameters ParameterKey=keyName,ParameterValue=CapeTownKey \
+--parameters ParameterKey=keyName,ParameterValue=CapeTownKeyPair \
 --region $REGION >> cfn-new-stack-id.json
 
